434961b77228443bf2219922dbb2673f56f495b8
Suraj <surajybtg@gmail.com>
2022-12-21 12:32:17 +0530

    Password Encrypted

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,8 +1,14 @@
+import bcrypt from "bcrypt";
 import User from "../model/user.js";
 
 export const signupUser = async (request, response) => {
   try {
-    const user = request.body;
+    const hashedPassword = await bcrypt.hash(request.body.password, 10);
+    const user = {
+      username: request.body.username,
+      name: request.body.name,
+      password: hashedPassword,
+    };
     const newUser = new User(user);
     await newUser.save();
     return response.status(200).json({ msg: "Signup successfull" });
--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+
 import Connection from "./database/db.js";
 import Router from "./routes/route.js";
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
+// for handling the wrong URL
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", Router);
 
 const port = 8080;
