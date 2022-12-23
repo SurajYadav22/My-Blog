92bf48f5306ba276cf9f41c71f6a07e173c617db
Suraj <surajybtg@gmail.com>
2022-12-23 08:57:40 +0530

    Check

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -30,5 +30,14 @@ export const API_NOTIFICATION_MESSAGES = {
 export const SERVICE_URLS = {
   userLogin: { url: "/login", method: "POST" },
   userSignup: { url: "/signup", method: "POST" },
-  uploadFile: { url: "/file/upload", method: "POST" },
+  getAllPosts: { url: "/posts", method: "GET", params: true },
+  getRefreshToken: { url: "/token", method: "POST" },
+  uploadFile: { url: "file/upload", method: "POST" },
+  createPost: { url: "create", method: "POST" },
+  deletePost: { url: "delete", method: "DELETE", query: true },
+  getPostById: { url: "post", method: "GET", query: true },
+  newComment: { url: "/comment/new", method: "POST" },
+  getAllComments: { url: "comments", method: "GET", query: true },
+  deleteComment: { url: "comment/delete", method: "DELETE", query: true },
+  updatePost: { url: "update", method: "PUT", query: true },
 };
--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from "../constants/config";
+import { getAccessToken } from "../utils/common-utils";
 
 const API_URL = "http://localhost:8080";
 
@@ -83,9 +84,9 @@ for (const [key, value] of Object.entries(SERVICE_URLS)) {
       url: value.url,
       data: value.method === "DELETE" ? "" : body,
       responseType: value.responseType,
-      // headers: {
-      //   authorization: getAccessToken(),
-      // },
+      headers: {
+        authorization: getAccessToken(),
+      },
 
       onUploadProgress: function (progressEvent) {
         if (showUploadProgress) {
