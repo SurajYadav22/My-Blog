d036fc97f0f3a8c95b560cf96d47e64a579129df
Suraj <surajybtg@gmail.com>
2022-12-23 08:58:27 +0530

    Ckeck server

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,10 @@
 import express from "express";
 import { uploadImage } from "../controller/image-controller.js";
+import {
+  authenticateToken,
+  createNewToken,
+} from "../controller/jwt-controller.js";
+import { createPost } from "../controller/post-controller.js";
 import { signupUser, loginUser } from "../controller/user-controller.js";
 
 import upload from "../utils/upload.js";
@@ -8,6 +13,9 @@ const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
-router.post("/file/upload", upload.single("file"), uploadImage);
+router.post("file/upload", upload.single("file"), uploadImage);
+router.post("/token", createNewToken);
+
+router.post("/create", authenticateToken, createPost);
 
 export default router;
